7d399eb3a70c298e1b6fc31005700d71c76a0609
Gafar <104564468+iGafar@users.noreply.github.com>
2023-06-17 14:50:24 +0300

    did exercises 5.1 and 5.2

---
+++ exercise5.1/main.js
@@ -0,0 +1,12 @@
+const sayHello = (
+  name = "Иван",
+  surname = "Иванов",
+  age = 10,
+  greeting = "Привет"
+) => {
+  alert(greeting + ", " + name + " " + surname + ", тебе " + age + " лет!");
+};
+
+sayHello();
+sayHello("Дима");
+sayHello("Евгений", "Петров", 25, "Здравствуйте");
